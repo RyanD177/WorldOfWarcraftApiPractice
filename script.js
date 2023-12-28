0f21f26f7a7dc54166cfa15d535b4613987f99c0
RyanD177 <w0477770@campus.nscc.ca>
2023-12-28 12:44:53 -0400

    zzzzzz

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-const API_URL =
-  "https://us.api.blizzard.com/data/wow/mount/index?namespace=static-us&locale=en_US&access_token=US9PlWTTKX9reWxrxSZT6t29lsEXMNImGf";
-const htmlElement = document.getElementById("text");
-
-async function getData() {
-  const response = await fetch(API_URL);
-  const data = await response.json();
-  const mounts = data.mounts;
-
-  const azureMounts = mounts.filter((mount) => mount.name.includes("Azure"));
+const API_BASE_URL = "https://us.api.blizzard.com/data/wow/pet/index";
+const NAMESPACE = "static-us";
+const LOCALE = "en_US";
+const ACCESS_TOKEN = "US9PlWTTKX9reWxrxSZT6t29lsEXMNImGf";
 
 
-  displayData(azureMounts);
+async function getItemData(itemId) {
+  const url = `https://us.api.blizzard.com/data/wow/pet/index?namespace=${NAMESPACE}&locale=${LOCALE}&access_token=${ACCESS_TOKEN}`;
+  const response = await fetch(url);
+
+   const data = await response.json();
+
+   const pets = data.pets;
+   console.log(pets);
+
+   pets.forEach((pet) => {
+      if(pet.name.includes('Rat')){
+         console.log(pet.name);
+      }
+   })
+
 }
 
-function displayData(mounts) {
-  mounts.forEach((mount) => {
-    const div = document.createElement("div");
-
-    div.classList.add("mounts-name");
-    div.textContent = mount.name;
-    htmlElement.appendChild(div);
-  });
-}
-
-getData();
+getItemData()
