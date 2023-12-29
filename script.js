aec1194bee1b8131b49782ff3e8e715a81876067
RyanD177 <w0477770@campus.nscc.ca>
2023-12-29 01:32:28 -0400

    getting the pet id

--- script.js
+++ script.js
@@ -4,20 +4,31 @@ const LOCALE = "en_US";
 const ACCESS_TOKEN = "US9PlWTTKX9reWxrxSZT6t29lsEXMNImGf";
 
 
-async function getItemData(itemId) {
+async function getItemData() {
   const url = `https://us.api.blizzard.com/data/wow/pet/index?namespace=${NAMESPACE}&locale=${LOCALE}&access_token=${ACCESS_TOKEN}`;
   const response = await fetch(url);
 
    const data = await response.json();
 
    const pets = data.pets;
+   const petID = data.pets.forEach((pet) => {
+      let pet_id = pet.id;
+      console.log(pet_id);
+      console.log(pet.name);
+   })
+
    console.log(pets);
 
    pets.forEach((pet) => {
       if(pet.name.includes('Rat')){
-         console.log(pet.name);
+    
       }
    })
+
+}
+
+function getDataMedia () {
+   const url = `https://us.api.blizzard.com/data/wow/media/pet/39?namespace=static-us&locale=en_US&access_token=US9PlWTTKX9reWxrxSZT6t29lsEXMNImGf`;
 
 }
 
