2c2d797ef60b73ce9dd26ccec5823f22a07db141
RyanD177 <w0477770@campus.nscc.ca>
2023-12-29 05:19:27 -0400

    pet images and stuff appended to petContainer

--- script.js
+++ script.js
@@ -1,35 +1,80 @@
 const API_BASE_URL = "https://us.api.blizzard.com/data/wow/pet/index";
-const NAMESPACE = "static-us";
+
+const NAMESPACE = "static-us"; /*
+These 3 variables are required by the Blizzard API when making a request.
+
+*/
 const LOCALE = "en_US";
 const ACCESS_TOKEN = "US9PlWTTKX9reWxrxSZT6t29lsEXMNImGf";
 
 
-async function getItemData() {
-  const url = `https://us.api.blizzard.com/data/wow/pet/index?namespace=${NAMESPACE}&locale=${LOCALE}&access_token=${ACCESS_TOKEN}`;
-  const response = await fetch(url);
+async function sleep(millis) {
+   return new Promise(resolve => setTimeout(resolve, millis));
+}
 
+
+
+async function getPetsAndPetID() {
+   const url = `https://us.api.blizzard.com/data/wow/pet/index?namespace=${NAMESPACE}&locale=${LOCALE}&access_token=${ACCESS_TOKEN}`;
+   const response = await fetch(url);
    const data = await response.json();
 
    const pets = data.pets;
-   const petID = data.pets.forEach((pet) => {
-      let pet_id = pet.id;
-      console.log(pet_id);
-      console.log(pet.name);
-   })
 
-   console.log(pets);
+    pets.forEach(async function(pet)  {
+      await sleep(100);
+      getMedia(pet.id, pet.name); 
+ 
+   });
+}
+async function getMedia(petId, petName) {
+   const url = `https://us.api.blizzard.com/data/wow/media/pet/${petId}?namespace=${NAMESPACE}&locale=${LOCALE}&access_token=${ACCESS_TOKEN}`;
+   try {
+      const response = await fetch(url);
+      const mediaData = await response.json();
+     const dragon  = mediaData.assets.some(asset => asset.value.includes('blue'));
 
-   pets.forEach((pet) => {
-      if(pet.name.includes('Rat')){
-    
+      if(dragon){
+         displayTheMedia(petName , mediaData.assets[0].value);
+         console.log(mediaData.assets[0].value);
       }
-   })
+   } catch (error) {
+      console.error('Error fetching media:', error);
+   }
+}
+
+
+function displayTheMedia (petName, imageURL) {
+
+let randomNumber  = Math.floor().Math.random();
+const petContainer = document.getElementById('pets');
+const petDiv = document.createElement('div');
+petDiv.classList.add('pet-div')
+
+const petText = document.createElement('h2');
+
+const petHealthText = document.createElement('p');
+
+petHealthText.textContent = randomNumber;
+petText.classList.add('pet-text');
+
+const image = document.createElement('img')
+image.src = imageURL;
+petText.textContent =`${petName}`
+petDiv.appendChild(petText)
+petDiv.appendChild(image)
+petDiv.appendChild(petHealthText)
+
+
+petContainer.appendChild(petDiv);
 
 }
 
-function getDataMedia () {
-   const url = `https://us.api.blizzard.com/data/wow/media/pet/39?namespace=static-us&locale=en_US&access_token=US9PlWTTKX9reWxrxSZT6t29lsEXMNImGf`;
 
-}
 
-getItemData()
+getPetsAndPetID();
+ 
+
+
+
+
